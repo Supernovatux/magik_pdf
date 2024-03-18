a6a7ffe34cb9f05d064c5449ee787202424434e7
Thulashitharan D <99125803+Supernovatux@users.noreply.github.com>
2024-03-18 18:05:46 +0530

    Update poppler_interface.rs
    Removed duplicate comments

--- src/poppler_interface.rs
+++ src/poppler_interface.rs
@@ -213,12 +213,6 @@ pub trait PopplerInterface {
                 .to_str()
                 .context("Invalid UTF-8 File name")?,
         );
-        // cache_path.push(format!("{}", num));
-        // while cache_path.is_dir() {
-        //     cache_path.pop();
-        //     num = rand::thread_rng().gen_range(0..100000);
-        //     cache_path.push(format!("{}", num));
-        // }
         let mut cmd = Command::new(path.as_path());
         if let Some(args) = args {
             cmd.args(args);
@@ -229,9 +223,6 @@ pub trait PopplerInterface {
             .arg(pdf_path)
             .arg(cache_path.clone())
             .output()?;
-        //println!("{}",output.status);
-        //println!("{}", String::from_utf8(output.stdout)?.trim_end());
-        //println!("{}", String::from_utf8(output.stderr)?.trim_end());
         let mut out_vec = Vec::new();
         for entry in glob::glob(&format!(
             "{}*.{}",
